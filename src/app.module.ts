594278e759ddbda8f973e257fd70d550b4d582ca
İlkcan Irtürk <ilkcan.irturk@outlook.com>
2025-11-04 14:28:10 +0300

    Edit app.module (remove PORT info from files)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config'; 
+import { TypeOrmModule } from '@nestjs/typeorm'; 
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], 
+      inject: [ConfigService], 
+      useFactory: (configService: ConfigService) => ({
+        type: 'mssql', 
+        host: configService.get<string>('DB_HOST')!, 
+        username: configService.get<string>('DB_USER')!, 
+        password: configService.get<string>('DB_PASS')!, 
+        database: configService.get<string>('DB_NAME')!, 
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, 
+        options: {
+          encrypt: false, 
+          trustServerCertificate: true, 
+        },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
